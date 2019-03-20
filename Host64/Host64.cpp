b3e9ed8a00192cb566883edbd3abf240af6e950b
mrworkman <workman.stephen@gmail.com>
2019-03-20 16:17:48 -0400

    Rename for consistency across projects.

--- Host64/Host64.cpp
+++ Host64/Host64.cpp
@@ -15,7 +15,7 @@
 // along with this program.If not, see<http://www.gnu.org/licenses/>.
 //
 
-#include "pch.h"
+#include "Stdafx.h"
 #include "Host64.h"
 #include "renfrew64_h.h"
 
--- Host64/Ping.cpp
+++ Host64/Ping.cpp
@@ -15,7 +15,7 @@
 // along with this program.If not, see<http://www.gnu.org/licenses/>.
 //
 
-#include "pch.h"
+#include "Stdafx.h"
 #include "renfrew64_h.h"
 
 int ping_Ping() {
